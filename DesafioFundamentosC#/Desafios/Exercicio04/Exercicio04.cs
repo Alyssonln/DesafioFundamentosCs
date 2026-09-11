86697760aba15f852f6b339836570d1d10e82cd8
Alyssonln <alyssonlnunes@gmail.com>
2026-09-11 13:03:26 -0300

    Exercicio04 finalizado.

---
+++ DesafioFundamentosC#/Desafios/Exercicio04/Exercicio04.cs
@@ -0,0 +1,20 @@
+﻿namespace DesafioFundamentosC_.Desafios.Exercicio04;
+
+public class Exercicio04
+{
+    public static void Executar()
+    {
+        Console.Write("Contador de caracteres: ");
+        string? contador = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(contador))
+        {
+            Console.WriteLine("Caractres: 0");
+        }
+        else
+        {
+            string tratandoContador = contador.Replace(" ", "").Trim();
+            Console.WriteLine($"Caracteres: {tratandoContador.Length}");
+        }
+    }
+}
--- DesafioFundamentosC#/Program.cs
+++ DesafioFundamentosC#/Program.cs
@@ -1,7 +1,9 @@
 ﻿using DesafioFundamentosC_.Desafios.Exercicio01;
 using DesafioFundamentosC_.Desafios.Exercicio02;
 using DesafioFundamentosC_.Desafios.Exercicio03;
+using DesafioFundamentosC_.Desafios.Exercicio04;
 
 Exercicio01.Executar();
 Exercicio02.Executar();
 Exercicio03.Executar();
+Exercicio04.Executar();
