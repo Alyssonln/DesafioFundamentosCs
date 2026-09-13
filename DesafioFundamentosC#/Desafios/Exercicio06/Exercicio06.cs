bf38884504f05a5ff2be31a0e2e32f8e9d0564c3
Alysson Nunes <alyssonlnunes@gmail.com>
2026-09-13 20:02:43 -0300

    Exercicio06

---
+++ DesafioFundamentosC#/Desafios/Exercicio06/Exercicio06.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+namespace DesafioFundamentosC_.Desafios.Exercicio06;
+
+public class Exercicio06
+{
+    public static void Executar()
+    {
+        var cultura = new CultureInfo("pt-BR");
+        DateTime agora = DateTime.Now;
+
+        Console.WriteLine("Escolha o formato de exibição da data:");
+        Console.WriteLine("1 - Formato completo (dia da semana, dia, mês, ano, hora, minutos, segundos)");
+        Console.WriteLine("2 - Apenas a data (dd/MM/yyyy)");
+        Console.WriteLine("3 - Apenas a hora (formato 24h)");
+        Console.WriteLine("4 - Data com o mês por extenso");
+        Console.Write("Opção: ");
+
+        string? opcao = Console.ReadLine();
+
+        switch (opcao)
+        {
+            case "1":
+                Console.WriteLine(agora.ToString("dddd, dd 'de' MMMM 'de' yyyy, HH:mm:ss", cultura));
+                break;
+
+            case "2":
+                Console.WriteLine(agora.ToString("dd/MM/yyyy"));
+                break;
+
+            case "3":
+                Console.WriteLine(agora.ToString("HH:mm:ss"));
+                break;
+
+            case "4":
+                Console.WriteLine(agora.ToString("dd 'de' MMMM 'de' yyyy", cultura));
+                break;
+
+            default:
+                Console.WriteLine("Opção inválida.");
+                break;
+        }
+    }
+}
--- DesafioFundamentosC#/Program.cs
+++ DesafioFundamentosC#/Program.cs
@@ -3,9 +3,11 @@ using DesafioFundamentosC_.Desafios.Exercicio02;
 using DesafioFundamentosC_.Desafios.Exercicio03;
 using DesafioFundamentosC_.Desafios.Exercicio04;
 using DesafioFundamentosC_.Desafios.Exercicio05;
+using DesafioFundamentosC_.Desafios.Exercicio06;
 
-Exercicio05.Executar();
 Exercicio01.Executar();
 Exercicio02.Executar();
 Exercicio03.Executar();
 Exercicio04.Executar();
+Exercicio05.Executar();
+Exercicio06.Executar();
